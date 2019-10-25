db9c198b2058f3e26510efca9acb75d9d6d003d6
황서연 <seoyeon5813@gmail.com>
2019-10-25 17:23:28 +0900

    커서 수정

--- cv_js.js
+++ cv_js.js
@@ -2,26 +2,26 @@
 
 
 /* cursor */ 
-var cursor = $('.cursor');
-var follower = $('.cursor_follower');
+// var cursor = $('.cursor');
+// var follower = $('.cursor_follower');
 
-mouseX = 0,
-mouseY = 0,
-positionX = 0,
-positionY = 0;
+// mouseX = 0,
+// mouseY = 0,
+// positionX = 0,
+// positionY = 0;
 
-$(document).mousemove(function(e){
-    mouseX = e.pageX;
-    mouseY = e.pageY;
+// $(document).mousemove(function(e){
+//     mouseX = e.pageX;
+//     mouseY = e.pageY;
 
-    /* cursor movement speed control */ 
-    positionX += (mouseX - positionX) / 15;
-    positionY += (mouseY - positionY) / 15;
+//     /* cursor movement speed control */ 
+//     positionX += (mouseX - positionX) / 15;
+//     positionY += (mouseY - positionY) / 15;
 
-    /* cursor movement */ 
-    cursor.css('left', mouseX).css('top', mouseY);
+//     /* cursor movement */ 
+//     cursor.css('left', mouseX).css('top', mouseY);
 
-});
+// });
 
 
 /* visual effect */ 
